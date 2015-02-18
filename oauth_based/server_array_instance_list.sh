b8b8cec9554e17447ed7bfa2e1b31398af94afde
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2015-02-18 12:08:04 -0600

    new script to list server array instances. Also adopts a nicer command line options.

---
+++ oauth_based/server_array_instance_list.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ $# -ne 3 ] 
+then
+	echo "USAGE: $0 API_ENDPOINT REFRESH_TOKEN SERVER_ARRAY_HREF_ID" 
+	echo "Where API_ENDPOINT is your API 1.5 endpoint found in the CM portal."
+	echo "Where REFRESH_TOKEN is your refresh token found in the CM portal."
+	echo "Where SERVER_ARRAY_HREF_ID is the ID number of the Server Array to list"
+	exit 1
+fi
+
+my_token_endpoint=${1}
+my_refresh_token=${2}
+server_array_id=${3}
+base_uri=`echo ${my_token_endpoint} | cut -d"/" -f1,2,3`
+
+access_token=`curl --include \
+  -H "X-API-Version:1.5" \
+  --request POST "$my_token_endpoint" \
+  -d "grant_type=refresh_token" \
+  -d "refresh_token=$my_refresh_token" |
+grep "access_token" | 
+cut -d":" -f 3 | 
+cut -d"," -f1 | 
+sed 's/\"//g'`
+
+curl --include \
+     -H "X-API-Version:1.5" \
+     -H "Authorization: Bearer $access_token" \
+     -X GET "${base_uri}/api/server_arrays/${server_array_id}/current_instances.xml"
